e897c2090313b77db81afc99081b898121dbbc70
AxelBelloir <axelbelloir1@gmail.com>
2026-04-07 09:12:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+=============== FLASK ===============
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route("/api/test", methods=["POST"])
+
+def test():
+    return jsonify({"message":"ca marche"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
