a2e512c5699050f67bdb1e365147df973ba8001c
AxelBelloir <axelbelloir1@gmail.com>
2026-04-07 09:11:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const API_URL = "https://cookie-clicker-l255.onrender.com/api";
+
+
+async function callAPI(body = {},endpoint){
+  try {
+        const response = await fetch(`${API_URL}/${endpoint}`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(body)
+        });
+        return await response.json();
+    } catch (e) {
+        console.error(`Erreur sur ${endpoint}:`, e);
+        notifier("Erreur de connexion au serveur");
+        return { message: "Erreur", logs: [] };
+    }
+}
+
 function test(){
-  document.getElementById("test").innerText = "test2";
+  res = callAPI({},"api/test")
+  document.getElementById("test").innerText = res.message;
 }
